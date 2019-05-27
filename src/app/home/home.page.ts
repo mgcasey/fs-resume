3b14653b357f0c750591adb1481bfdf60a6e94f2
Michael Casey <meghancasey@Michaels-MacBook-Pro.local>
2019-05-28 00:33:32 +0100

    Resume progress

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,57 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+//  constructor() {}
+
+  public myDateOfBirth: String;
+
+  ionViewDidEnter() {
+    this.myDateOfBirth = "1999-12-20";
+  }
+  constructor(public alertController: AlertController, public toastController: ToastController) {}
+
+  
+  
+  async presentAlertMultipleButtons() {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      subHeader: 'Cannot be diskliked.',
+      message: 'Please see button above.',
+      buttons: ['Cancel', 'Delete']
+    });
+
+    await alert.present();
+  }
+
+  async presentToastWithOptions() {
+    const toast = await this.toastController.create({
+      header: 'Thanks for favoriting me!',
+      message: 'Click to Close',
+      position: 'top',
+      buttons: [
+        {
+          side: 'start',
+          icon: 'star',
+          text: 'Favorite',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        }, {
+          text: 'Done',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
+
+
+  pressMe() {
+    alert("Thanks for stopping by my site!")
+  }
+  
 
 }
